3eb740c24ce3967e4e46794542fe9634a0a00851
incog <noelsaido50@gmail.com>
2023-06-19 15:14:11 +0300

    Add multiple line on pages

--- main.py
+++ main.py
@@ -11,7 +11,8 @@ for index, row in df.iterrows():
     pdf.set_font(family="Times", style="B", size=24)
     pdf.set_text_color(100, 100, 100)
     pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1)
-    pdf.line(10, 21, 200, 21)
+    for y in range(20, 298, 10):
+        pdf.line(10, y, 200, y)
 
     # Set footer
     pdf.ln(265)
@@ -26,6 +27,9 @@ for index, row in df.iterrows():
         pdf.set_font(family="Times", style="I", size=8)
         pdf.set_text_color(180, 180, 180)
         pdf.cell(w=0, h=12, txt=row["Topic"], align="R", ln=1)
+
+        for y in range(20, 298, 10):
+            pdf.line(10, y, 200, y)
 
 
 pdf.output("output.pdf")
